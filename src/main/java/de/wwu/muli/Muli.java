44cb6121c068372b4d45e6e66d2db5d31a96574c
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-19 11:21:29 +0200

    Initial commit

---
+++ src/main/java/de/wwu/muli/Muli.java
@@ -0,0 +1,16 @@
+package de.wwu.muli;
+
+public class Muli {
+
+    public static Solution[] search(Find b, SearchStrategy c, Supplier<Boolean> searchArea) {
+        // TODO: iterate through multiple results of searchArea.get() == true => not anymore!
+        // TODO: maybe this is only a stub? or is the inner one a stub? ... hm
+        return null;
+    }
+
+    // TODO: maybe add intermediate type representing the (continuable) search space
+    // TODO: should be something like public static <A> Solution<A>[] muli(A labelling, Supplier<Boolean> searchArea)
+    //public static Solution[] muli(int labelling, Supplier<Boolean> searchArea) {
+    //	return null;
+    //}
+}
---
+++ src/main/java/de/wwu/muli/SearchStrategy.java
@@ -0,0 +1,5 @@
+package de.wwu.muli;
+
+enum SearchStrategy {
+    IterativeDeepening
+}
