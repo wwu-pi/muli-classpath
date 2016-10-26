9e2adb38eb262ab2cd4ffd863b338f4a09d5a9a9
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-26 09:59:07 +0200

    Add fail,  muli to public API

--- src/main/java/de/wwu/muli/Muli.java
+++ src/main/java/de/wwu/muli/Muli.java
@@ -1,15 +1,20 @@
 package de.wwu.muli;
 
+import java.util.function.Supplier;
+
 public class Muli {
+
+    public static native void fail();
 
     public static Solution[] search(Find b, SearchStrategy c, Supplier<Boolean> searchArea) {
         // TODO: iterate through multiple results of searchArea.get() == true => not anymore!
         // TODO: maybe this is only a stub? or is the inner one a stub? ... hm
-        return null;
+        return new Solution[]{Muli.muli(searchArea)};
     }
 
+    public native static Solution muli(Supplier<Boolean> searchArea);
+
     // TODO: maybe add intermediate type representing the (continuable) search space
-    // TODO: should be something like public static <A> Solution<A>[] muli(A labelling, Supplier<Boolean> searchArea)
     //public static Solution[] muli(int labelling, Supplier<Boolean> searchArea) {
     //	return null;
     //}
