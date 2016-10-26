e48ff85e8285328acf873fa7e669e16bd0df7416
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-26 09:56:36 +0200

    Make API public

--- src/main/java/de/wwu/muli/Find.java
+++ src/main/java/de/wwu/muli/Find.java
@@ -1,5 +1,5 @@
 package de.wwu.muli;
 
-enum Find {
+public enum Find {
     All, First
 }
--- src/main/java/de/wwu/muli/SearchStrategy.java
+++ src/main/java/de/wwu/muli/SearchStrategy.java
@@ -1,5 +1,5 @@
 package de.wwu.muli;
 
-enum SearchStrategy {
+public enum SearchStrategy {
     IterativeDeepening
 }
