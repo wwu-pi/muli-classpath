df2b02958ec5b9b2af4e6ca705db5a79d2034715
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-28 16:16:23 +0200

    Do not prescribe result type of Supplier

--- src/main/java/de/wwu/muli/Muli.java
+++ src/main/java/de/wwu/muli/Muli.java
@@ -4,15 +4,25 @@ import java.util.function.Supplier;
 
 public class Muli {
 
-    public static native void fail();
+    public static native MuliFailException fail();
 
-    public static Solution[] search(Find b, SearchStrategy c, Supplier<Boolean> searchArea) {
+    public static Solution[] search(Find b, SearchStrategy c, Supplier searchArea) {
         // TODO: iterate through multiple results of searchArea.get() == true => not anymore!
         // TODO: maybe this is only a stub? or is the inner one a stub? ... hm
-        return new Solution[]{Muli.muli(searchArea)};
+        if (b == Find.First) {
+            return new Solution[]{Muli.muli(searchArea)};
+        } else {
+            throw new IllegalArgumentException("not supported yet. :(");
+        }
     }
 
-    public native static Solution muli(Supplier<Boolean> searchArea);
+    public static Solution[] search(Find b, Supplier searchArea) {
+        // TODO: iterate through multiple results of searchArea.get() == true => not anymore!
+        // TODO: maybe this is only a stub? or is the inner one a stub? ... hm
+        return search(b, SearchStrategy.IterativeDeepening, searchArea);
+    }
+
+    public native static Solution muli(Supplier searchArea);
 
     // TODO: maybe add intermediate type representing the (continuable) search space
     //public static Solution[] muli(int labelling, Supplier<Boolean> searchArea) {
