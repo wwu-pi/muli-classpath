9b6bdb75d1ba39b25ad2744ad37f35a04d27162f
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2016-10-28 16:15:48 +0200

    Add explanatory comment to MuliFailException

--- src/main/java/de/wwu/muli/MuliFailException.java
+++ src/main/java/de/wwu/muli/MuliFailException.java
@@ -1,7 +1,9 @@
 package de.wwu.muli;
 
 /**
- * Created by j_dage01 on 28.10.16.
+ * Exception that signifies an invalid result. Created by Muli.fail()
+ * (or maybe not, because fail() is handled by Muggl/Muli-Env. Anyway,
+ * this is a declaration of an exception to keep compilers happy.)
  */
 public class MuliFailException extends RuntimeException {
 }
