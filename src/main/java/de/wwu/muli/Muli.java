f7d9c0eb7276f6e1bcbe6fbe3175f3b827fe4129
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-01-31 16:25:35 +0100

    WIP attempt at multiple solutions

--- src/main/java/de/wwu/muli/Muli.java
+++ src/main/java/de/wwu/muli/Muli.java
@@ -9,10 +9,11 @@ public class Muli {
     public static Solution[] search(Find b, SearchStrategy c, Supplier searchArea) {
         // TODO: iterate through multiple results of searchArea.get() == true => not anymore!
         // TODO: maybe this is only a stub? or is the inner one a stub? ... hm
+        Solution[] solutions = Muli.muli(searchArea);
         if (b == Find.First) {
-            return new Solution[]{Muli.muli(searchArea)};
+            return new Solution[]{solutions[0]};
         } else {
-            throw new IllegalArgumentException("not supported yet. :(");
+            return solutions;
         }
     }
 
@@ -20,15 +21,30 @@ public class Muli {
         return search(b, SearchStrategy.IterativeDeepening, searchArea);
     }
 
-    public static Solution muli(Supplier searchArea) {
+    public static Solution[] muli(Supplier searchArea) {
+        // Find out prior VM state in order to reset it after executing the current search area
         ExecutionMode previousMode = getVMExecutionMode();
+        int previousRecursionLevel = getVMRecursionLevel();
+
+        // Execute search area symbolically
         setVMExecutionMode(ExecutionMode.SYMBOLIC);
+        setVMRecursionLevel(previousRecursionLevel + 1);
         Object retval = searchArea.get(); // Maybe transfer call to VM? this should be easy
+        Solution[] solutions = getVMSolutions(getVMRecursionLevel());
+
+        // Return to previous VM state
+        setVMRecursionLevel(previousRecursionLevel);
         setVMExecutionMode(previousMode);
-        // TODO use retval or maybe get solutions from VM?
-        // TODO consider multiple executions of get() in case of backtracking!
-        return new Solution();
+
+        return solutions;
+        // consider multiple executions of get() in case of backtracking!
+        // --> no, this is implicit. As long as there are choice points in `searchArea', all their possible choices are evaluated
     }
+
+    private static native Solution[] getVMSolutions(int level);
+
+    private static native int getVMRecursionLevel();
+    private static native void setVMRecursionLevel(int level);
 
     private static native ExecutionMode getVMExecutionMode();
     private static native void setVMExecutionMode(ExecutionMode mode);
