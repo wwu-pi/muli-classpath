c316420b9bc1231048a1a5dc0a7d3a6adc26a2dd
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2017-03-06 17:29:11 +0100

    Added native methods (WIP)

--- src/main/java/de/wwu/muli/Muli.java
+++ src/main/java/de/wwu/muli/Muli.java
@@ -4,8 +4,6 @@ import java.util.function.Supplier;
 
 public class Muli {
 
-    public static native MuliFailException fail();
-
     public static Solution[] search(Find b, SearchStrategy c, Supplier searchArea) {
         // TODO: iterate through multiple results of searchArea.get() == true => not anymore!
         // TODO: maybe this is only a stub? or is the inner one a stub? ... hm
@@ -28,17 +26,23 @@ public class Muli {
             Object retval = searchArea.get(); // Maybe transfer call to VM? this should be easy
             recordSolutionAndBacktrackVM(retval);
         } catch(Throwable e) {
-            recordSolutionAndBacktrackVM(e);
+            recordExceptionAndBacktrackVM(e);
         }
         setVMExecutionMode(previousMode); // Restore previous state of VM
 
-
-
-        return getVMRecordedSolutions();
+        return getVMRecordedSolutions(); // TODO -> array.
     }
+
+    public static native MuliFailException fail();
 
     private static native ExecutionMode getVMExecutionMode();
     private static native void setVMExecutionMode(ExecutionMode mode);
+
+    private static native void setVMSymbolicExecutionTreeRoot();
+    private static native void recordSolutionAndBacktrackVM(Object solution);
+    private static native void recordExceptionAndBacktrackVM(Throwable exception);
+
+    private static native Solution getVMRecordedSolutions();
 
     // TODO: maybe add intermediate type representing the (continuable) search space
     //public static Solution[] muli(int labelling, Supplier<Boolean> searchArea) {
