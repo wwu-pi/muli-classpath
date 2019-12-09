5afb9b8178ada642065510d2fa98166b055d7b96
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-12-09 13:01:13 +0100

    Add placeholder for free variables
    ... to be picked up by the runtime and replaced by an actual free variable.

---
+++ src/main/java/de/wwu/muli/freevar/FreeVariablePlaceholder.java
@@ -0,0 +1,9 @@
+package de.wwu.muli.freevar;
+
+/**
+ * The purpose of this class is to be used in muli-lang as a placeholder for any kind of free variable,
+ * and to be picked up by the runtime and replaced by an actual free variable.
+ */
+@SuppressWarnings("unused")
+public class FreeVariablePlaceholder {
+}
